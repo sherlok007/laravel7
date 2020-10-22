6980bf4beea579002adb0ab7abcb3fde518cb124
sherlok007 <mymotifxf6@gmail.com>
2020-10-22 18:32:37 +0530

    Some changes made to insert excel data properly

--- app/Imports/TodosImport.php
+++ app/Imports/TodosImport.php
@@ -22,12 +22,13 @@ class TodosImport implements ToModel, WithHeadingRow
         $date = $date->format('Y-m-d');
 
         return new Todo([
-            'order_no'     => $row['order_no'],
-            'buyer_name'    => $row['buyer_name'],
+            'order_no' => $row['order_no'],
+            'buyer_name' => ucwords($row['buyer_name']),
             'buyer_phone' => $row['buyer_phone'],
-            'buyer_address' => $row['buyer_address'],
+            'buyer_address' => ucwords($row['buyer_address']),
             'consign_no' => $row['consign_no'],
             'order_date' => $date,
+            'refund_applied' => empty($row['refund_applied']) ? 0 : 1,
         ]);
     }
 }
